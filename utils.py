3b8332f67a0dad2e0823c3f788196f3994ac602a
ImJustBlurr <morningstartariq1105@gmail.com>
2025-08-06 21:16:34 -0400

    created utils.py for helper functions

---
+++ utils.py
@@ -0,0 +1,18 @@
+import os
+import stat
+
+def log_error(e):
+    print(f"{type(e).__name__} at line {e.__traceback__.tb_lineno} of {__file__}: {e}")
+
+def is_file_readonly(path):
+    return not os.access(path, os.W_OK)
+
+def set_file_readonly(path, readonly=True):
+    if os.path.isfile(path):
+        current_permissions = os.stat(path).st_mode
+        if readonly:
+            os.chmod(path, current_permissions & ~stat.S_IWUSR & ~stat.S_IWGRP & ~stat.S_IWOTH)
+        else:
+            os.chmod(path, current_permissions | stat.S_IWUSR)
+        return True
+    return False
